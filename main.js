9d2fba65870508732c75ccd9013d281300e16564
ahmad4everyone <ahmadmut24@gmail.com>
2024-01-13 12:18:22 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+var ola = document.getElementById("para")
+var ade = document.getElementById("dspl")
+function msg(){
+    if(ola ==='' || ''){
+        alert('please read about me')
+    }
+    ade.style.display = 'block', ola.style.display = 'none';
+}
+
+    
